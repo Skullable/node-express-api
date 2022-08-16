3c4f85d8373c7e789c67028fecb568299a7ba0e7
Skullable <riyaanbinusman57@gmail.com>
2022-08-16 16:47:43 +0500

    linking to mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from 'body-parser';
 
 import userRoutes from './routes/users.js';
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 5000;
@@ -9,6 +10,9 @@ const PORT = 5000;
 app.use(bodyParser.json());
 
 app.use('/users', userRoutes )
+
+mongoose.connect('mongodb+srv://Skullable:@123123@cluster0.j3qumzt.mongodb.net/?retryWrites=true&w=majority', () => 
+console.log('connected to DB!'))
 
 app.get('/', (req, res) => res.send('Hello from hompage.'));
 
